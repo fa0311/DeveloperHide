5ac4489b111fdc54a3f0e59e596eb26883016612
ふぁ <yuki@yuki0311.com>
2023-12-11 08:54:14 +0900

    remove activityCloseCallback and update flag
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- app/src/main/java/io/github/takusan23/developerhide/ShortcutHostActivity.kt
+++ app/src/main/java/io/github/takusan23/developerhide/ShortcutHostActivity.kt
@@ -17,16 +17,7 @@ import io.github.takusan23.developerhide.tool.USBDebug
  * */
 class ShortcutHostActivity : AppCompatActivity() {
 
-    private val activityCloseCallback = registerForActivityResult(ActivityCloseContracts()) {
-        // アプリ終了時にUSBデバッグをONに戻す
-        USBDebug.setUSBDebugSetting(contentResolver, true)
-        // 開発者向けオプションもOFFにする設定なら戻す
-        if (USBDebug.isSetDevelopmentOptionHide(this)) {
-            USBDebug.setDevelopmentSetting(contentResolver, true)
-        }
-        // バックキーで戻ってきたらここに来る。アプリ閉じる
-        finishAndRemoveTask()
-    }
+    private val activityCloseCallback = registerForActivityResult(ActivityCloseContracts()) {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,7 +33,7 @@ class ShortcutHostActivity : AppCompatActivity() {
         val packageName = intent.getStringExtra("package_name")!!
         val launchIntent = packageManager.getLaunchIntentForPackage(packageName)?.apply {
             categories?.add(Intent.CATEGORY_LAUNCHER) // ランチャーから起動した時はこれが付与されるので真似するように
-            flags = 0 // 勝手に Intent.FLAG_ACTIVITY_NEW_TASK を指定するので消す
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
         }
         activityCloseCallback.launch(launchIntent)
 
